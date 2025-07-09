f08fc53f77e58a4d561d032233a3be8b290f2811
Samarth DevTools <samarthm@devtools.in>
2025-07-09 16:38:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+let person = {
+    name: 'Samarth',
+    age: 23,
+    isEmployee: true,
+    address: {
+        city: 'Bangalore',
+        state: 'Karnataka'
+    }
+}
+
+console.log(person.name);
+console.log(person.address.city);
+console.log(person['name']);
+
+
+let company={
+    name: 'DevTools',
+    loc: 'Banglore',
+    employee: ['Abc','Def','Ghi']
+}
+
+console.log(company.name);
+
